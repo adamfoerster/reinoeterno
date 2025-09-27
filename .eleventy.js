17dc75f4f0dd9d9d2a60fda45166605c1f67b3e1
Adam Foerster <adam.foerster@pm.me>
2025-09-27 14:49:52 -0300

    feat: build from obsidian vault directly

--- .eleventy.js
+++ .eleventy.js
@@ -1,11 +1,14 @@
 
-module.exports = function(eleventyConfig) {
+module.exports = function (eleventyConfig) {
   const path = require("path");
-  eleventyConfig.addPassthroughCopy("src/assets");
+  eleventyConfig.addPassthroughCopy("assets");
   eleventyConfig.addGlobalData("layout", "layout.njk");
-  eleventyConfig.addFilter("urlencode", function(str) {
+  eleventyConfig.addFilter("urlencode", function (str) {
     return encodeURIComponent(str);
   });
+  eleventyConfig.addFilter("remove_underline", function (str){
+    return str.replaceAll("_", " ");
+  })
   eleventyConfig.addGlobalData("eleventyComputed", {
     permalink: data => {
       if (data.published === false || !data.published) {
@@ -14,12 +17,34 @@ module.exports = function(eleventyConfig) {
       return data.permalink ?? data.page.filePathStem + "/index.html";
     }
   });
+  eleventyConfig.addCollection("tagList", function (collectionApi) {
+    let tagSet = new Set();
+
+    collectionApi.getAll()
+      .forEach(item => {
+        if (item.data.published === false || !item.data.published) return;
+
+        if ("tags" in item.data) {
+          let tags = item.data.tags;
+          if (typeof tags === "string") {
+            tags = [tags];
+          }
+          tags
+            .filter(tag => !["all", "nav", "post"].includes(tag)) // ignora tags internas
+            .forEach(tag => tagSet.add(tag));
+        }
+      }
+    );
+
+    return [...tagSet].sort();
+  });
+
 
   return {
     dir: {
       input: "../../Documents/MegaSync/obisidian-vault/",
       output: "./dist",
-      includes: "../../../projects/reinoeterno/src/_includes/"
+      includes: "../../../projects/reinoeterno/_includes/"
     },
     markdownTemplateEngine: "njk",
     htmlTemplateEngine: "njk"
